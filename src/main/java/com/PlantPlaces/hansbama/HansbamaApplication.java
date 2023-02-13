f2a9d69c8317358a210200f94a8223dfdec13e44
Administrator <Administrator@DESKTOP-099UQRG>
2023-02-12 21:18:31 -0500

    Initial commit

---
+++ src/main/java/com/PlantPlaces/hansbama/HansbamaApplication.java
@@ -0,0 +1,13 @@
+package com.PlantPlaces.hansbama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HansbamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HansbamaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PlantPlaces/hansbama/HansbamaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PlantPlaces.hansbama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HansbamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
